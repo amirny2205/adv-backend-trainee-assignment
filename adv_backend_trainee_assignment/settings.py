d41ec6a392e8ee5ae23cb2233b4d9841f337fb29
amirny2205 <amirny2205@yandex.ru>
2022-10-17 13:57:51 +0300

    moved to django-environ

--- adv_backend_trainee_assignment/settings.py
+++ adv_backend_trainee_assignment/settings.py
@@ -10,24 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from dotenv import load_dotenv
+import environ
 import os
 from pathlib import Path
 
 
-load_dotenv()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("Debug")
+DEBUG = env("Debug")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -82,11 +88,11 @@ WSGI_APPLICATION = 'adv_backend_trainee_assignment.wsgi.application'
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
-       'NAME': os.getenv("DATABASE_NAME"),
-       'USER': os.getenv("DATABASE_USER"),
-       'PASSWORD': os.getenv("DATABASE_PASSWORD"),
-       'HOST': os.getenv("DATABASE_HOST"),
-       'PORT': os.getenv("DATABASE_PORT"),
+       'NAME': env("DATABASE_NAME"),
+       'USER': env("DATABASE_USER"),
+       'PASSWORD': env("DATABASE_PASSWORD"),
+       'HOST': env("DATABASE_HOST"),
+       'PORT': env("DATABASE_PORT"),
    }
 }
 
